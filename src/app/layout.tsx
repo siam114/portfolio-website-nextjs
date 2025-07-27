b9c89dbb548dfd7fc5b1a104d3c3709509f7a400
siam114 <smsiamislam598@gmail.com>
2025-07-28 03:13:35 +0600

    demo navbar nad footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>navbar</div>
         {children}
+        <footer>Footer</footer>
       </body>
     </html>
   );
