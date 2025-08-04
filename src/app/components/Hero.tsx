c2c2682ea27f24b00331a890774efc454be8ac2c
siam114 <smsiamislam598@gmail.com>
2025-08-04 20:09:01 +0600

    design profile photo

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <section className="py=28 container mx-auto max-w-7xl px-4">
       <div className="max-w-3xl mx-auto text-center">
-        <div>
-            <Image src="/siam.jpg" alt="Profile Photo" width={100} height={100}/>
+        <div className="flex flex-col items-center mb-4">
+            <Image src="/siam.jpg" alt="Profile Photo" width={1000} height={1000} className="rounded-full mb-4 w-36 h-36 object-cover ring-2 ring-primary"/>
         </div>
       </div>
     </section>
