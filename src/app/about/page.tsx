d8c5a4bbefc7cc9123ead314306a1a1fc15d7591
siam114 <smsiamislam598@gmail.com>
2025-08-07 21:10:37 +0600

    formatig about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,12 @@
 const About = () => {
-    return (
-        <div className="container mx-auto max-w-7xl py-10">
-            <h1 className="text-3xl font-bold text-center mb-10">About Me</h1>
+  return (
+    <div className="container mx-auto max-w-7xl py-10">
+      <h1 className="text-3xl font-bold text-center mb-10">About Me</h1>
 
-            {/* bio section */}
-            <section></section>
-        </div>
-    )
-}
+      {/* bio section */}
+      <section></section>
+    </div>
+  );
+};
 
 export default About;
