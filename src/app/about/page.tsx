946c141de4b140f093761f59404a8d6b80d7972f
siam114 <smsiamislam598@gmail.com>
2025-08-07 21:10:09 +0600

    design bio section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,9 @@ const About = () => {
     return (
         <div className="container mx-auto max-w-7xl py-10">
             <h1 className="text-3xl font-bold text-center mb-10">About Me</h1>
+
+            {/* bio section */}
+            <section></section>
         </div>
     )
 }
