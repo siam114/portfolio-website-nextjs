394e331c58b609773cbc4347d3a8d164af1f4d7e
siam114 <smsiamislam598@gmail.com>
2025-08-07 20:10:09 +0600

    added footer on layout section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { ThemeProvider } from "./context/ThemeContext";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,7 @@ export default function RootLayout({
         <ThemeProvider>
           <Navbar />
           <main className="min-h-screen pt-24">{children}</main>
-          <footer>Footer</footer>
+          <Footer/>
         </ThemeProvider>
       </body>
     </html>
