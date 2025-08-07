6d9974e7e0f977c37da4a8b92b010ef6636f878e
siam114 <smsiamislam598@gmail.com>
2025-08-08 02:44:43 +0600

    complete skill section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,5 @@
+import { FaCode, FaLaptopCode } from "react-icons/fa";
+
 const About = () => {
   return (
     <div className="container mx-auto max-w-7xl py-10">
@@ -15,6 +17,40 @@ const About = () => {
       {/* skill section */}
       <section className="mb-16">
           <h2 className="section-title">Skills</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md ">
+              <FaCode className="h-8 w-8 text-primary mb-4"/>
+              <h3 className="text-xl font-semibold mb-2">Frontend</h3>
+              <ul className="text-secondary space-y-2">
+                <li>React / Next.js</li>
+                <li>TypeScript</li>
+                <li>Tailwind CSS</li>
+                <li>HTML5 / CSS3</li>
+              </ul>
+            </div>
+
+             <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md ">
+              <FaLaptopCode className="h-8 w-8 text-primary mb-4"/>
+              <h3 className="text-xl font-semibold mb-2">Backend</h3>
+              <ul className="text-secondary space-y-2">
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>MongoDB</li>
+                <li>JavaScript (ES6+)</li>
+              </ul>
+            </div>
+
+             <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md ">
+              <FaLaptopCode className="h-8 w-8 text-primary mb-4"/>
+              <h3 className="text-xl font-semibold mb-2">Tools & Others</h3>
+              <ul className="text-secondary space-y-2">
+                <li>Git / Githud</li>
+                <li>Postman </li>
+                <li>Prettier</li>
+                <li>Figma / Pixso</li>
+              </ul>
+            </div>
+          </div>
       </section>
     </div>
   );
