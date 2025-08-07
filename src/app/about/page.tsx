2d0ad2ccf3939f0ddaf5d883176f9f7b642b6ce6
siam114 <smsiamislam598@gmail.com>
2025-08-07 21:04:00 +0600

    design heading on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,8 @@
 const About = () => {
     return (
-        <div>About page</div>
+        <div className="container mx-auto max-w-7xl py-10">
+            <h1 className="text-3xl font-bold text-center mb-10">About Me</h1>
+        </div>
     )
 }
 
