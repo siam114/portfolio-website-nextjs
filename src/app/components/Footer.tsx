53c6d2777d151c106a6d99df9da51be52c17536b
siam114 <smsiamislam598@gmail.com>
2025-08-07 20:48:53 +0600

    design footer in profile

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="bg-white dark:bg-dark border-t border-gray-200 dark:border-gray-800">
+      <div className="container mx-auto max-w-7xl px-4 py-8">
+        <div className="flex flex-col md:flex-row justify-center items-center">
+          <div className="mb-4 md:mb-0">
+            <Link href="/" className="text-xl font-bold text-primary">
+              SM SIAM
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
