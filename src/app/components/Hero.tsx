2487692e14d6161db35e14e0c2f8c140ebb910ca
siam114 <smsiamislam598@gmail.com>
2025-08-12 16:45:41 +0600

    hero image added framer-motion

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import { motion } from 'framer-motion';
+import { scaleIn } from "@/utils/animation";
 
 const Hero = () => {
   return (
     <section className="py-10 container mx-auto max-w-7xl px-4">
-      <div className="max-w-3xl mx-auto text-center">
-        <div className="flex flex-col items-center mb-4">
+      <div
+      className="max-w-3xl mx-auto text-center">
+        <motion.div {...scaleIn} transition={{delay: 0.4}} className="flex flex-col items-center mb-4">
           <Image
             src="/siam.jpg"
             alt="Profile Photo"
@@ -14,7 +19,7 @@ const Hero = () => {
             height={1000}
             className="rounded-full mb-4 w-36 h-36 object-cover ring-2 ring-primary"
           />
-        </div>
+        </motion.div>
 
         <h1 className="text-2xl md:text-4xl font-bold mb-4">
           Hi, I&apos;m <span className="text-primary"> SM Sumsuzzaman</span>
